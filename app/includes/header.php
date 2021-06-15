d12849c013050fd7ac2326a6c79046526c237467
Issam EL FERKH <issam.elferkh@gmail.com>
2021-06-15 14:47:06 +0100

    start UI

--- app/includes/header.php
+++ app/includes/header.php
@@ -1,41 +1,64 @@
-<?php
-	// include("session.php");
 
-?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.83.1">
+    <title>CCM - Cluster Chimie & Matériaux</title>
 
-<!DOCTYPE HTML>
-<html>
-	<head>
-		<title></title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="assets/css/main.css" />
-		<!-- Data Tables -->
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/">
 
-		<script src="assets/js/jquery-3.6.0.min.js"></script>
-		<link rel="stylesheet" type="text/css" href="assets/DataTables/datatables.css"/>
-		<!-- <link rel="stylesheet" type="text/css" href="assets/DataTables/css/dataTables.bootstrap4.min.css"/> -->
-		<script type="text/javascript" src="assets/DataTables/datatables.min.js"></script>
-		<!-- <script type="text/javascript" src="assets/DataTables/js/dataTables.bootstrap4.min.js"></script> -->
+    <!-- Bootstrap Icones -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <!-- <link href="/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous"> -->
 
-	</head>
-	<body class="is-preload">
+    <!-- Favicons -->
+<link rel="apple-touch-icon" href="/docs/5.0/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+<link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+<link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+<link rel="manifest" href="/docs/5.0/assets/img/favicons/manifest.json">
+<link rel="mask-icon" href="/docs/5.0/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
+<link rel="icon" href="/docs/5.0/assets/img/favicons/favicon.ico">
+<meta name="theme-color" content="#7952b3">
 
-		<!-- Wrapper -->
-			<div id="wrapper">
 
-				<!-- Main -->
-					<div id="main">
-						<div class="inner">
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
 
-							<!-- Header -->
-								<header id="header">
-									<a href="#" class="logo"><strong>App Name</strong></a>
-									<ul class="icons">
-										<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-										<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-										<li><a href="#" class="icon brands fa-snapchat-ghost"><span class="label">Snapchat</span></a></li>
-										<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-										<li><a href="#" class="icon brands fa-medium-m"><span class="label">Medium</span></a></li>
-									</ul>
-								</header>
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="./assets/css/dashboard.css" rel="stylesheet">
+  </head>
+  <body>
+    
+<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Cluster Chimie & Matériaux</a>
+  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <!-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> -->
+  <ul class="navbar-nav px-3">
+    <li class="nav-item text-nowrap">
+      <a class="nav-link" href="./../index.php">Sign out</a>
+    </li>
+  </ul>
+</header>
--- app/index.php
+++ app/index.php
@@ -1,8 +1,62 @@
 <?php include("./includes/header.php"); ?>
-
-<section>
-
-</section>
-
-<!-- Sidebar -->
-<?php include("./includes/sidebar.php"); ?>
+<div class="container-fluid">
+  <div class="row">
+  <?php include("./includes/navbar.php"); ?>
+  <!-- Start Container -->
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Dashboard</h1>
+      </div>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+          <div class="col">
+              <div class="card">
+                <div class="card-header"><i class="bi bi-archive"> Folder_01</i></div>
+                <div class="card-body text-secondary">
+                  <h5 class="card-title">A brief description of Folder_01</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <i class="bi bi-person-circle"> presnom.nom@um6p.ma</i>
+                  <a href="sub_folder_l1.php" class="stretched-link"></a>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card">
+                <div class="card-header"><i class="bi bi-archive"> Folder_02</i></div>
+                <div class="card-body text-secondary">
+                  <h5 class="card-title">A brief description of Folder_02</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <i class="bi bi-person-circle"> presnom.nom@um6p.ma</i>
+                  <a href="#" class="stretched-link"></a>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card">
+                <div class="card-header"><i class="bi bi-archive"> Folder_03</i></div>
+                <div class="card-body text-secondary">
+                  <h5 class="card-title">A brief description of Folder 03</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <i class="bi bi-person-circle"> presnom.nom@um6p.ma</i>
+                  <a href="#" class="stretched-link"></a>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card">
+                <div class="card-header"><i class="bi bi-archive"> Folder_04</i></div>
+                <div class="card-body text-secondary">
+                  <h5 class="card-title">A brief description of Folder_04</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <i class="bi bi-person-circle"> presnom.nom@um6p.ma</i>
+                  <a href="#" class="stretched-link"></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+    <!-- End Container -->
+  </div>
+</div>
+<?php include("./includes/footer.php"); ?>
